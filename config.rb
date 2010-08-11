63a999ceb57ac2dad1f8542642b00d72435f35ba
Kuniaki IGARASHI <igaiga@gmail.com>
2010-08-11 18:10:11 +0900

    twym get a feature for twitter oauth.

--- config.rb
+++ config.rb
@@ -8,3 +8,9 @@ IRC = :irc
 TWITTER = :twitter
 TUPLE_AVAILAVLE_TIME = 30 #sec
   #↑この時間以上経過したメッセージは表示しない
+
+# twitter
+TWITTER_OAUTH_CONSUMER_KEY    = 'Fwt207Mk1k6SPACXrVoK6Q' # for twym
+TWITTER_OAUTH_CONSUMER_SECRET = 'eTYqz70dB3QLcuU5RBJCGyW6VJogz32PBQXteZ5Y' # for twym
+TWITTER_URL = 'http://twitter.com'
+TWITTER_OAUTH_CONFIG_FILE = 'config_twitter_oauth_access_token.rb'
---
+++ twitter_oauth_authorize.rb
@@ -0,0 +1,46 @@
+#! ruby -Ku
+# -*- coding: utf-8; -*-
+$:.unshift File.join(File.dirname(__FILE__))
+
+require 'rubygems'
+require 'oauth'
+require 'json'
+require 'config.rb'
+
+class OAuthAuthorizer
+  def self.run
+    consumer = OAuth::Consumer.new(
+                                   TWITTER_OAUTH_CONSUMER_KEY,
+                                   TWITTER_OAUTH_CONSUMER_SECRET, {
+                                     :site => TWITTER_URL })
+
+    request_token = consumer.get_request_token
+    puts "The page have been opened in browser is for OAuth. "
+    puts "URL : " + request_token.authorize_url
+    system "open #{request_token.authorize_url}"
+
+    puts 'input the oauth PIN code : (7-digit number. for example : 0123456)'
+    pin = gets
+    pin = pin.chomp!.slice(/^.{0,7}/)
+
+    access_token = request_token.get_access_token(:oauth_verifier => pin)
+    token        = access_token.token
+    secret       = access_token.secret
+
+    json = { :token => token, :secret => secret }.to_json
+    File.open(TWITTER_OAUTH_CONFIG_FILE, "w") { |f|
+      f.puts json
+    }
+
+  end
+end
+
+
+=begin
+# test code
+OAuthAuthorizer.run
+at = File.read(TWITTER_OAUTH_CONFIG_FILE)
+hash = JSON.parse(at)
+p token  = hash['token']
+p secret = hash['secret']
+=end
