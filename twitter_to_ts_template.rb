cf4b057031ecb25f199739b79b91508200db1170
Kuniaki IGARASHI <igaiga@gmail.com>
2011-03-20 19:18:39 +0900

    using twitter stream with Basic auth

--- twitter_to_ts_template.rb
+++ twitter_to_ts_template.rb
@@ -8,51 +8,60 @@ require 'rubygems'
 require 'json'
 require 'oauth'
 
+require "twitter/json_stream"
+require "eventmachine"
+
 require 'twitter_oauth_authorize.rb'
 
-#HASHTAG = '#kosenconf'
-#HASHTAG = '#glt'
-#HASHTAG = '#twym'
-HASHTAG = '#nowplaying'
+#HASHTAG = 'kosenconf'
+#HASHTAG = 'glt'
+#HASHTAG = 'twym'
+#HASHTAG = 'nowplaying'
+HASHTAG = 'iphone'
 
-#twym
 require 'drb'
 require 'config.rb'
 $ts = DRbObject.new_with_uri(TS_URL)
 
 # for OAuth
-consumer = OAuth::Consumer.new(TWITTER_OAUTH_CONSUMER_KEY, TWITTER_OAUTH_CONSUMER_SECRET, {
-     :site               => TWITTER_URL,
-     :http_method        => :post })
+# consumer = OAuth::Consumer.new(TWITTER_OAUTH_CONSUMER_KEY, TWITTER_OAUTH_CONSUMER_SECRET, {
+#      :site               => TWITTER_URL,
+#      :http_method        => :post })
 
-unless (File.exist?(TWITTER_OAUTH_CONFIG_FILE))
-  # ユーザーにOAuth認証をしてもらう
-  OAuthAuthorizer.run
-end
+# unless (File.exist?(TWITTER_OAUTH_CONFIG_FILE))
+#   # ユーザーにOAuth認証をしてもらう
+#   OAuthAuthorizer.run
+# end
 
-at = JSON.parse(File.read(TWITTER_OAUTH_CONFIG_FILE))
-oauth_access_token  = at['token']
-oauth_access_secret = at['secret']
-access_token = OAuth::Token.new(oauth_access_token, oauth_access_secret)
+# at = JSON.parse(File.read(TWITTER_OAUTH_CONFIG_FILE))
+# oauth_access_token  = at['token']
+# oauth_access_secret = at['secret']
+# access_token = OAuth::Token.new(oauth_access_token, oauth_access_secret)
 
+query = URI.encode(HASHTAG)
+#query = ARGV.shift
+#raise "please give some query" if query.nil?
 
-uri = URI.parse('http://stream.twitter.com/1/statuses/filter.json')
-Net::HTTP.start(uri.host, uri.port) do |http|
-  request = Net::HTTP::Post.new(uri.request_uri)
-  request.set_form_data('track' => HASHTAG)
-  request.oauth!(http, consumer, access_token)
-  http.request(request) do |response|
-    raise 'Response is not chuncked' unless response.chunked?
-    response.read_body do |chunk|
-      status = JSON.parse(chunk) rescue next
-      next unless status['text']
-      user = status['user']
-      puts "#{user['screen_name']}: #{status['text']}"
+#BASIC_認証
+twitter_id   = "username"
+twitter_pass = "password"
+
+EventMachine::run {
+  EventMachine::defer {
+    stream = Twitter::JSONStream.connect(
+               :path => "/1/statuses/filter.json?track=#{query}",
+                                         :auth => "#{twitter_id}:#{twitter_pass}"
+             )
+
+    stream.each_item do |status|
+      tweet = JSON.parse(status)      
+      screen_name = tweet['user']['screen_name']
+      body = tweet['text']
+      puts "#{screen_name}: #{body}"
       mb = Hash.new
-      mb[NICK] = user['screen_name']
-      mb[MESSAGE] = status['text'].gsub(/#{HASHTAG}/i,'')
+      mb[NICK] = screen_name
+      mb[MESSAGE] = body.gsub(/#{query}$/i,'')
       $ts.write([TWITTER, mb], TUPLE_AVAILAVLE_TIME)
     end
-  end
-end
-
+  }
+}
